307bf35e5b41b2340f3e5b3a2e8971fc4dd3a733
Bconstantlee <bconstantlee@gmail.com>
2017-02-27 23:55:20 -0500

    Update

---
+++ TwitterDemo/Tweet.swift
@@ -0,0 +1,13 @@
+//
+//  Tweet.swift
+//  TwitterDemo
+//
+//  Created by Bconsatnt on 2/27/17.
+//  Copyright © 2017 Bconsatnt. All rights reserved.
+//
+
+import UIKit
+
+class Tweet: NSObject {
+
+}
---
+++ TwitterDemo/tweetCell.swift
@@ -0,0 +1,24 @@
+//
+//  tweetCell.swift
+//  TwitterDemo
+//
+//  Created by Bconsatnt on 2/27/17.
+//  Copyright © 2017 Bconsatnt. All rights reserved.
+//
+
+import UIKit
+
+class tweetCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
